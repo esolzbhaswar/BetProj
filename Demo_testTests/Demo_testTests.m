a84859767fa0396af09db68d97822e41cfc33668
cyrusesolz <cyrus@esolz.com>
2015-09-30 11:26:36 +0530

    first commit

---
+++ Demo_testTests/Demo_testTests.m
@@ -0,0 +1,34 @@
+//
+//  Demo_testTests.m
+//  Demo_testTests
+//
+//  Created by Bhaswar's MacBook Air on 13/03/15.
+//  Copyright (c) 2015 Esolz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Demo_testTests : XCTestCase
+
+@end
+
+@implementation Demo_testTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
